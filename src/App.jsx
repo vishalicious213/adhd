947644fffa8f2fbce51ac0278d8cb3e05d1434f1
vishalicious213 <vishalicious213@gmail.com>
2024-03-20 16:12:56 -0700

    drafted Question and rendered in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Instructions from './components/Instructions'
+import Question from './components/Question'
 
 const App = () => {
     return (
         <>
             <Instructions />
+            <Question />
         </>
     )
 }

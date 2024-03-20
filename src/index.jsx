6a15a49aeb7ed3c552b818f2bd4f1d14ccda8154
vishalicious213 <vishalicious213@gmail.com>
2024-03-20 13:24:26 -0700

    initial commit

---
+++ src/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+const Index = () => {
+    return (
+        <div>index</div>
+    )
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<Index />)
+
+export default Index

607cf554f58424dba9b0b8f2da5de412690206ba
vishalicious213 <vishalicious213@gmail.com>
2024-03-20 16:27:01 -0700

    Question: destructured props

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 
-const Question = () => {
+const Question = ({ question, answer }) => {
     return (
-        <div>Question</div>
+        <>
+            <div>{question}</div>
+        </>
     )
 }
 

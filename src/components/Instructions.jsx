3a39e597d82a10447ed69808ae705fb74be1c213
vishalicious213 <vishalicious213@gmail.com>
2024-03-20 15:22:59 -0700

    imported inter font and applied font styles to h1 and p in Instructions

--- src/components/Instructions.jsx
+++ src/components/Instructions.jsx
@@ -4,7 +4,7 @@ const Instructions = () => {
     return (
         <>
             <h1>Adult ADHD Self-Report Scale (ASRS v1.1) Symptom Checklist</h1>
-            <p>Please answer the questions below, rating yourself on each of the criteria shown. As you answer each question, check the box that best describes how you have felt and conducted yourself over the past 6 months. Please sumbit this completed checklist when you are done and a copy will be sent to your email which you can share with your healthcare professional.</p>
+            <p className='italics'>Please answer the questions below, rating yourself on each of the criteria shown. As you answer each question, check the box that best describes how you have felt and conducted yourself over the past 6 months. Please sumbit this completed checklist when you are done and a copy will be sent to your email which you can share with your healthcare professional.</p>
         </>
     )
 }

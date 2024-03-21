8bdd67c307c8ad22076eac55ada5ece372d4ed16
vishalicious213 <vishalicious213@gmail.com>
2024-03-20 21:42:23 -0700

    moved questionsArray from App into /data/questions.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,7 @@
 import React from 'react'
 import Instructions from './components/Instructions'
 import Question from './components/Question'
-
-const questionsArray = [
-    {
-        id: 1,
-        text: "How often do you have trouble wrapping up the final details of a project, once the challenging parts have been done?",
-    },
-    {
-        id: 2,
-        text: "How often do you have difficulty getting things in order when you have to do a task that requires organization?",
-    },
-    {
-        id: 3,
-        text: "How often do you have problems remembering appointments or obligations?",
-    },
-    {
-        id: 4,
-        text: "When you have a task that requires a lot of thought, how often do you avoid or delay getting started?",
-    },
-    {
-        id: 5,
-        text: "How often do you fidget or squirm with your hands or feet when you have to sit down for a long time?",
-    },
-    {
-        id: 6,
-        text: "How often do you feel overly active and compelled to do things, like you were driven by a motor? "
-    },
-]
+import { questionsArray } from './data/questions'
 
 const App = () => {
     return (
---
+++ src/data/questions.js
@@ -0,0 +1,26 @@
+export const questionsArray = [
+    {
+        id: 1,
+        text: "How often do you have trouble wrapping up the final details of a project, once the challenging parts have been done?",
+    },
+    {
+        id: 2,
+        text: "How often do you have difficulty getting things in order when you have to do a task that requires organization?",
+    },
+    {
+        id: 3,
+        text: "How often do you have problems remembering appointments or obligations?",
+    },
+    {
+        id: 4,
+        text: "When you have a task that requires a lot of thought, how often do you avoid or delay getting started?",
+    },
+    {
+        id: 5,
+        text: "How often do you fidget or squirm with your hands or feet when you have to sit down for a long time?",
+    },
+    {
+        id: 6,
+        text: "How often do you feel overly active and compelled to do things, like you were driven by a motor? "
+    },
+]

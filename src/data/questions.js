8a86dcaffeff5bb047c5001f9aca6d0ff69b61dd
vishalicious213 <vishalicious213@gmail.com>
2024-03-20 21:45:16 -0700

    added questions to questions.js

--- src/data/questions.js
+++ src/data/questions.js
@@ -21,6 +21,30 @@ export const questionsArray = [
     },
     {
         id: 6,
-        text: "How often do you feel overly active and compelled to do things, like you were driven by a motor? "
+        text: "How often do you feel overly active and compelled to do things, like you were driven by a motor?"
+    },
+    {
+        id: 7,
+        text: "How often do you make careless mistakes when you have to work on a boring or difficult project?"
+    },
+    {
+        id: 8,
+        text: "How often do you have difficulty keeping your attention when you are doing boring or repetitive work?"
+    },
+    {
+        id: 9,
+        text: "How often do you have difficulty concentrating on what people say to you, even when they are speaking to you directly?"
+    },
+    {
+        id: 10,
+        text: "How often do you misplace or have difficulty finding things at home or at work?"
+    },
+    {
+        id: 11,
+        text: "How often are you distracted by activity or noise around you?"
+    },
+    {
+        id: 12,
+        text: "How often do you leave your seat in meetings or other situations in which you are expected to remain seated?"
     },
 ]

a51aa9fcc8dab2a11418bd52adf6916d9886639f
vishalicious213 <vishalicious213@gmail.com>
2024-03-25 15:17:33 -0700

    Score: logging score value when score button is clicked

--- src/components/Score.jsx
+++ src/components/Score.jsx
@@ -6,28 +6,30 @@ const Score = ({ scores }) => {
         let score = 0
 
         if (scores.answer1 === "sometimes" || scores.answer1 === "often" || scores.answer1 === "very-often") {
-            console.log(scores.answer1)
+            score = score + 1
         }
 
         if (scores.answer2 === "sometimes" || scores.answer2 === "often" || scores.answer2 === "very-often") {
-            console.log(scores.answer2)
+            score = score + 1
         }
 
         if (scores.answer3 === "sometimes" || scores.answer3 === "often" || scores.answer3 === "very-often") {
-            console.log(scores.answer3)
+            score = score + 1
         }
 
         if (scores.answer4 === "often" || scores.answer4 === "very-often") {
-            console.log(scores.answer4)
+            score = score + 1
         }
 
         if (scores.answer5 === "often" || scores.answer5 === "very-often") {
-            console.log(scores.answer5)
+            score = score + 1
         }
 
         if (scores.answer6 === "often" || scores.answer6 === "very-often") {
-            console.log(scores.answer6)
+            score = score + 1
         }
+
+        console.log("Score", score)
     }
     return (
         <>

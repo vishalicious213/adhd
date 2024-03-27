02e4be32a7c1dadeff3350266fb6da2116c10e37
vishalicious213 <vishalicious213@gmail.com>
2024-03-27 16:48:01 -0700

    /landing/index.jsx: rendering blank screen or asrs1 based on button click

--- src/components/landing/index.jsx
+++ src/components/landing/index.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import { useState } from 'react'
 import Asrs1 from '../asrs1'
 
 const Index = () => {
+    const [selected, setSelected] = useState("home")
+
+    function handleClick(choice) {
+        console.log(choice)
+        setSelected(choice)
+    }
+
     return (
         <div>
-            <Asrs1 />
+            <button onClick={() => handleClick("home")}>HOME</button>
+            <button onClick={() => handleClick("asrs1")}>ASRS v1.1</button>
+
+            <div>{
+                selected === "home" ? "" :
+                selected === "asrs1" ? <Asrs1 /> :
+                ""
+                }
+            </div>
         </div>
     )
 }

4c2806eb51d4ebde5ce91c4b8d3bd32b5787c917
vishalicious213 <vishalicious213@gmail.com>
2024-03-27 19:09:48 -0700

    RenderSection: set up props for def, tools, links

--- src/components/landing/RenderSection.jsx
+++ src/components/landing/RenderSection.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 
-const RenderSection = () => {
+const RenderSection = ({ def="Definition", tools="Tools", links="Links" }) => {
     return (
-        <div>RenderSection</div>
+        <section className="section">
+            <div className="definition text">{def}</div>
+            <div className="tools">{tools}</div>
+            <div className="links text">{links}</div>
+        </section>
     )
 }
 
--- src/components/landing/index.jsx
+++ src/components/landing/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useState } from 'react'
 import Asrs1 from '../asrs1'
+import RenderSection from './RenderSection'
 
 const Index = () => {
     const [selected, setSelected] = useState("home")
@@ -14,6 +15,12 @@ const Index = () => {
         <div>
             <button onClick={() => handleClick("home")}>HOME</button>
             <button onClick={() => handleClick("asrs1")}>ASRS v1.1</button>
+
+            <RenderSection
+                // def=""
+                // tools=""
+                // links=""
+            />
 
             <div>{
                 selected === "home" ? "" :

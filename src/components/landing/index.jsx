d0d5c2096cd8c7ef6ac111a58ba496e8dec278d2
vishalicious213 <vishalicious213@gmail.com>
2024-03-27 23:18:11 -0700

    moved ADHD section array from /landing/index to /data/sectionsAdhd.js

--- src/components/landing/index.jsx
+++ src/components/landing/index.jsx
@@ -2,26 +2,7 @@ import React from 'react'
 import { useState } from 'react'
 import Asrs1 from '../asrs1'
 import RenderSection from './RenderSection'
-
-const sectionsArray = [
-    {
-        name: "ADHD",
-        def: "ADHD is one of the most common neurodevelopmental disorders of childhood. It is usually first diagnosed in childhood and often lasts into adulthood. Children with ADHD may have trouble paying attention, controlling impulsive behaviors (may act without thinking about what the result will be), or be overly active.",
-        tools: [
-            {
-                button: "ASRS v1.1", 
-                choice: "asrs1", 
-                title: "Adult ADHD Self-Report Scale (ASRS-v1.1) Symptom Checklist",
-                desc: "The Symptom Checklist is an instrument consisting of 18 DSM-IV-TR criteria. Six of the eighteen questions were found to be the most predictive of symptoms consistent with ADHD. The remaining twelve provide additional cues and can serve as further probes into possible symptoms."}, 
-            {
-                button: "ASRS v5", 
-                choice: "asrs5", 
-                title: "Adult ADHD Self-Report Scale (ASRS-v5) Symptom Checklist",
-                desc: "The ASRS5 is a self report questionnaire designed to assess symptoms of Attention Deficit Hyperactivity Disorder (ADHD) in adults. It consists of 6 questions related to symptoms of inattention and hyperactivity/impulsivity. The ASRS5 is an updated version of the ASRSv1.1."}
-        ],
-        links: "adhd links"
-    }
-]
+import { sectionsAdhdArray } from '../../data/sectionsAdhd'
 
 const Index = () => {
     const [selected, setSelected] = useState("home")
@@ -36,7 +17,7 @@ const Index = () => {
             <button onClick={() => handleClick("home")}>HOME</button>
             <button onClick={() => handleClick("asrs1")}>ASRS v1.1</button>
 
-            {sectionsArray.map(section => (
+            {sectionsAdhdArray.map(section => (
                 <RenderSection
                     key={section.name}
                     name={section.name}

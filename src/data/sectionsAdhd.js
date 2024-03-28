950d531fc379b3ee92460223666d672e37f78fa8
vishalicious213 <vishalicious213@gmail.com>
2024-03-28 00:34:57 -0700

    updated data in sectionsAdhdArray

--- src/data/sectionsAdhd.js
+++ src/data/sectionsAdhd.js
@@ -1,18 +1,18 @@
 export const sectionsAdhdArray = [
     {
         name: "ADHD",
-        def: "ADHD is one of the most common neurodevelopmental disorders of childhood. It is usually first diagnosed in childhood and often lasts into adulthood. Children with ADHD may have trouble paying attention, controlling impulsive behaviors (may act without thinking about what the result will be), or be overly active.",
+        def: "ADHD (Attention Deficit Hyperactivity Disorder) is one of the most common neurodevelopmental disorders of childhood. It is usually first diagnosed in childhood and often lasts into adulthood. Children with ADHD may have trouble paying attention, controlling impulsive behaviors (may act without thinking about what the result will be), or be overly active.",
         tools: [
             {
                 button: "ASRS v1.1", 
                 choice: "asrs1", 
                 title: "Adult ADHD Self-Report Scale (ASRS-v1.1) Symptom Checklist",
-                desc: "The Symptom Checklist is an instrument consisting of 18 DSM-IV-TR criteria. Six of the eighteen questions were found to be the most predictive of symptoms consistent with ADHD. The remaining twelve provide additional cues and can serve as further probes into possible symptoms."}, 
+                desc: "The ASRS v1.1 is an instrument consisting of 18 DSM-IV-TR criteria. Six of the eighteen questions were found to be the most predictive of symptoms consistent with ADHD. The remaining twelve provide additional cues and can serve as further probes into possible symptoms."}, 
             {
                 button: "ASRS v5", 
                 choice: "asrs5", 
                 title: "Adult ADHD Self-Report Scale (ASRS-v5) Symptom Checklist",
-                desc: "The ASRS5 is a self report questionnaire designed to assess symptoms of Attention Deficit Hyperactivity Disorder (ADHD) in adults. It consists of 6 questions related to symptoms of inattention and hyperactivity/impulsivity. The ASRS5 is an updated version of the ASRSv1.1."}
+                desc: "The ASRS5 is a self-report questionnaire designed to assess symptoms of ADHD in adults. It consists of 6 questions related to symptoms of inattention and hyperactivity / impulsivity. It is an updated version of the ASRS v1.1."}
         ],
         links: "adhd links"
     }

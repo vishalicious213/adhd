e1c463ed7f887748f83d0bbb593cc2a54cc20039
vishalicious213 <vishalicious213@gmail.com>
2024-03-29 13:30:50 -0700

    drafted /data/asrs5.js

---
+++ src/components/asrs5/Form.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { useState } from 'react'
+import Question from './Question'
+// import Score from './Score'
+import { questionsArray } from '../../data/questions'
+
+const Form = () => {
+    const [formData, setFormData] = useState({
+        answer1: "",
+        answer2: "",
+        answer3: "",
+        answer4: "",
+        answer5: "",
+        answer6: "",
+    })
+
+    function handleChange(event) {
+        const { name, value } = event.target
+        setFormData(prevFormData => {
+            return {
+                ...prevFormData,
+                [name]: value,
+            }
+        })
+    }
+
+    return (
+        <>
+            {questionsArray.map(q => <Question 
+                question={q.text} 
+                num={q.id} 
+                key={q.id} 
+                checked={Object.values(formData)[q.id - 1]}
+                handleChange={handleChange}
+            />)}
+
+            {/* <Score scores={formData} /> */}
+        </>
+    )
+}
+
+export default Form

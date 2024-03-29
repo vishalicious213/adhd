1d3c7db232ba5920d0bc34778d516a70cd5f75cd
vishalicious213 <vishalicious213@gmail.com>
2024-03-29 13:22:13 -0700

    drafted /asrs5/Instructions.jsx

---
+++ src/components/asrs5/Instructions.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Instructions = () => {
+    return (
+        <>
+            <h1>Adult ADHD Self-Report Screening Scale for DSM-5 (ASRS v5)</h1>
+            <p className='text italics'>This Adult ADHD Self-Report Screening Scale for DSM-5 (ASRS-5) is intended for people aged 18 years or older. Check the box that best describes how your patient has felt over the past 6 months. Score each item in the range 0-4 and give everyone a summary score of 0-24. A score of 14 or higher is suggestive of ADHD (91.4% sensitivity, 96.0% specificity).</p>
+        </>
+    )
+}
+
+export default Instructions

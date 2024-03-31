83833775cd36e138ded8b1962046410d509148ae
vishalicious213 <vishalicious213@gmail.com>
2024-03-31 14:46:12 -0700

    asrs1/Score: improved logic to make sure that all 6 questions are completed before a rating is given when button is clicked. went from individual if statements to looping through answers and used .includes to check answers against multiple values

--- src/components/asrs1/Score.jsx
+++ src/components/asrs1/Score.jsx
@@ -11,31 +11,20 @@ const Score = ({ scores }) => {
         let unfinishedQuestions = 6
         setComplete(false)
 
-
-        if (scores.answer1 === "sometimes" || scores.answer1 === "often" || scores.answer1 === "very-often") {
-            adhdScore = adhdScore + 1            
+        // loop through answers and increment score appropriately
+        for (let i = 0; i < 6; i++) {
+            if (i < 3) {
+                if (["sometimes", "often", "very-often"].includes(scoresArray[i])) {
+                    adhdScore++
+                }
+            } else {
+                if (["often", "very-often"].includes(scoresArray[i])) {
+                    adhdScore++
+                }
+            }
         }
 
-        if (scores.answer2 === "sometimes" || scores.answer2 === "often" || scores.answer2 === "very-often") {
-            adhdScore = adhdScore + 1
-        }
-
-        if (scores.answer3 === "sometimes" || scores.answer3 === "often" || scores.answer3 === "very-often") {
-            adhdScore = adhdScore + 1
-        }
-
-        if (scores.answer4 === "often" || scores.answer4 === "very-often") {
-            adhdScore = adhdScore + 1
-        }
-
-        if (scores.answer5 === "often" || scores.answer5 === "very-often") {
-            adhdScore = adhdScore + 1
-        }
-
-        if (scores.answer6 === "often" || scores.answer6 === "very-often") {
-            adhdScore = adhdScore + 1
-        }
-
+        // make sure that all questions are answered
         scoresArray.forEach(scoreValue => {
             if (["never", "rarely", "sometimes", "often", "very-often"].includes(scoreValue)) {
                 unfinishedQuestions--

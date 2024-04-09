66a2ccd9f6d6332e8b219dffd7782e447a276b41
vishalicious213 <vishalicious213@gmail.com>
2024-04-08 17:34:48 -0700

    updated /data/sectionsAdhd.js with SASI info

--- src/data/sectionsAdhd.js
+++ src/data/sectionsAdhd.js
@@ -7,12 +7,20 @@ export const sectionsAdhdArray = [
                 button: "ASRS v1.1", 
                 choice: "asrs1", 
                 title: "Adult ADHD Self-Report Scale (ASRS-v1.1) Symptom Checklist",
-                desc: "The ASRS v1.1 is an instrument consisting of 18 DSM-IV-TR criteria. Six of the eighteen questions were found to be the most predictive of symptoms consistent with ADHD. The remaining twelve provide additional cues and can serve as further probes into possible symptoms."}, 
+                desc: "The ASRS v1.1 is an instrument consisting of 18 DSM-IV-TR criteria. Six of the eighteen questions were found to be the most predictive of symptoms consistent with ADHD. The remaining twelve provide additional cues and can serve as further probes into possible symptoms."
+            }, 
             {
                 button: "ASRS v5", 
                 choice: "asrs5", 
                 title: "Adult ADHD Self-Report Scale (ASRS-v5) Symptom Checklist",
-                desc: "The ASRS5 is a self-report questionnaire designed to assess symptoms of ADHD in adults. It consists of 6 questions related to symptoms of inattention and hyperactivity / impulsivity. It is an updated version of the ASRS v1.1."}
+                desc: "The ASRS5 is a self-report questionnaire designed to assess symptoms of ADHD in adults. It consists of 6 questions related to symptoms of inattention and hyperactivity / impulsivity. It is an updated version of the ASRS v1.1."
+            },
+            {
+                button: "SASI", 
+                choice: "sasi", 
+                title: "Women's ADHD Self-Assessment Symptom Inventory (SASI)",
+                desc: "Developed by two clinicians who focused on adult ADHD in women. SASI is not a diagnostic assessment. It gives insight into the types of challenges a woman with ADHD could experience. Its use is recommended as part of a structured interview in conjunction with other diagnostic tools."
+            }
         ],
         links: [
             {

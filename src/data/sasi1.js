dd5c16c8dc98b46bf658648a1e14596ff62a9624
vishalicious213 <vishalicious213@gmail.com>
2024-04-09 09:51:30 -0700

    drafted /data/sasi1.js

---
+++ src/data/sasi1.js
@@ -0,0 +1,26 @@
+export const sasi1Questions = [
+    {
+        id: 1,
+        text: "I daydreamed a lot in school",
+    },
+    {
+        id: 2,
+        text: "My mind wandered, even when I tried to listen to the teacher",
+    },
+    {
+        id: 3,
+        text: "In class, I didn't hear the teacher's instructions",
+    },
+    {
+        id: 4,
+        text: "I made careless mistakes on tests",
+    },
+    {
+        id: 5,
+        text: "I often lost or misplaced things",
+    },
+    {
+        id: 6,
+        text: "I got teased for being 'spacey'"
+    }
+]

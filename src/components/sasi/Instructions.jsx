7567080f2cd22bfb5eef962bbf66bb606c3c853c
vishalicious213 <vishalicious213@gmail.com>
2024-04-09 11:07:22 -0700

    Sasi1 is able to map over specific ranges of questions and render them. I need to build out a specific render component next and then add all of the questions and their sections and render them

--- src/components/sasi/Instructions.jsx
+++ src/components/sasi/Instructions.jsx
@@ -9,10 +9,10 @@ const Instructions = ({ onclick }) => {
             <h3>Directions:</h3>
             <p className="text italics no-border">Mark each item below with one of the code numbers to show how much that feeling or behavior is part of your personal experience.</p>
             <ol className="text bold no-top-margin" start="0">
-                <li>That's not at all like me; that almost never happens to me.</li>
-                <li>That's a little like me; that happens to me, but not very often.</li>
-                <li>That's a lot like me; that happens to me often.</li>
-                <li>That's just like me; that happens to me almost all the time.</li>
+                <li>That's not at all like me; that almost never happens to me</li>
+                <li>That's a little like me; that happens to me, but not very often</li>
+                <li>That's a lot like me; that happens to me often</li>
+                <li>That's just like me; that happens to me almost all the time</li>
             </ol>
             <p className="text italics no-border">When an item does not pertain to you, such as a parenting question for a woman without children, simply leave that item blank.</p>
             <p className="text italics no-border">When you encounter an item about an issue you can't recall or about which you have no knowledge, use a question mark (?) to respond.</p>

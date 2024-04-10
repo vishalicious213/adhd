99622a8c3d6074be4ae03d5e21bb19f9a3de31a7
vishalicious213 <vishalicious213@gmail.com>
2024-04-10 12:44:41 -0700

    styled SASI sections, categories and last-child of each question

--- src/components/render/RenderSasiQuestions.jsx
+++ src/components/render/RenderSasiQuestions.jsx
@@ -4,15 +4,15 @@ const RenderSasiQuestions = ({data, onchange, state}) => {
     return (
         data.map(category => 
             <div key={category.name}>
-                <h2>{category.section}</h2>
-                <h3>{category.name}</h3>
+                {category.section && <h2 className="sasi-section">{category.section}</h2>}
+                <h3 className="sasi-category">{category.name}</h3>
                 <div className="sasi-container">
                     {
                         category.questions.map((q, index) => {
                             let item = `${category.stateName}${index}`
 
                             return (
-                                <div key={index}>
+                                <div key={index} className="sasi-question">
                                     <select 
                                         id={item}
                                         value={state[item]}

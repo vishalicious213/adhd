75821b59c05787405398791611bfa13267bf6e0f
vishalicious213 <vishalicious213@gmail.com>
2024-04-11 12:28:14 -0700

    /sasi/index: imported NavContext and used it in SasiFooter

--- src/components/sasi/index.jsx
+++ src/components/sasi/index.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
-
+import { useContext } from 'react'
+import { NavContext } from '../landing'
 import Instructions from './Instructions'
 import SasiFooter from './SasiFooter'
 
 const Sasi = ({ onclick }) => {
+    const { selected, handleClick } = useContext(NavContext)
     return (
         <>
             <Instructions onclick={onclick} />
-            <SasiFooter onclick={onclick} />
+            <SasiFooter onclick={handleClick} />
         </>
     )
 }

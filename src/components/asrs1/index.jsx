eee9d9d1e194138f1e879e2cb496fa6f0712bc4a
vishalicious213 <vishalicious213@gmail.com>
2024-04-16 16:33:57 -0700

    ASRS1: created asrs1Context in index and wrapped ASRS1 components with it. sharing state from the context

--- src/components/asrs1/index.jsx
+++ src/components/asrs1/index.jsx
@@ -1,15 +1,43 @@
+import React from 'react'
+import { useState, createContext } from "react"
+
 import Instructions from "./Instructions"
 import Form from "./Form"
 
-import React from 'react'
+const Asrs1Context = createContext()
 
 const Asrs1 = () => {
+    const [asrs1Data, setAsrs1Data] = useState({
+        answer1: "",
+        answer2: "",
+        answer3: "",
+        answer4: "",
+        answer5: "",
+        answer6: "",
+        answer7: "",
+        answer8: "",
+        answer9: "",
+        answer10: "",
+        answer11: "",
+        answer12: "",
+        answer13: "",
+        answer14: "",
+        answer15: "",
+        answer16: "",
+        answer17: "",
+        answer18: "",
+    })
+
     return (
-        <>
+        <Asrs1Context.Provider value={{
+            asrs1Data,
+            setAsrs1Data
+        }}>
             <Instructions />
             <Form />
-        </>
+        </Asrs1Context.Provider>
     )
 }
 
 export default Asrs1
+export { Asrs1Context }

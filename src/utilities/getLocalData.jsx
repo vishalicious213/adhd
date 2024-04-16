b18a1f5ef45f6feaad5c98d237748d6b9d905111
vishalicious213 <vishalicious213@gmail.com>
2024-04-15 22:06:44 -0700

    getLocalData: drafted remove()

--- src/utilities/getLocalData.jsx
+++ src/utilities/getLocalData.jsx
@@ -9,12 +9,22 @@ const LocalData = ({ name, data }) => {
     function load(name) {
         let savedData = JSON.parse(localStorage.getItem(name))
         console.log("LOAD FROM LOCALSTORAGE", savedData)
+        console.log("real data", localStorage.getItem(name))
+        console.log("fake data", localStorage.getItem("name"))
+        if (localStorage.getItem(name)) {console.log("there's data")}
+    }
+
+    function remove(name) {
+        console.log("DELETING FROM LOCALSTORAGE")
+        localStorage.removeItem(name)
+        console.log(localStorage.getItem(name))
     }
 
     return (
         <div id="score">
             <button className="score-btn" onClick={() => save(name, data)}>SAVE</button>
             <button className="score-btn" onClick={() => load(name)}>LOAD</button>
+            <button className="score-btn" onClick={() => remove(name)}>DELETE</button>
         </div>
     )
 }

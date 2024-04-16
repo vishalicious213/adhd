dab956a5d98c39497717d71120a5b5c68305a36c
vishalicious213 <vishalicious213@gmail.com>
2024-04-16 11:37:08 -0700

    asrs5: form data can be deleted and if so, form state is cleared/reset

--- src/utilities/saveLocalData.jsx
+++ src/utilities/saveLocalData.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
-const resetAsrs1State = { answer1: "", answer2: "", answer3: "", answer4: "", answer5: "", answer6: "", answer7: "", answer8: "", answer9: "", answer10: "", answer11: "", answer12: "", answer13: "", answer14: "", answer15: "", answer16: "", answer17: "", answer18: "", }
+const resetAsrs1State = { answer1: "", answer2: "", answer3: "", answer4: "", answer5: "", answer6: "", answer7: "", answer8: "", answer9: "", answer10: "", answer11: "", answer12: "", answer13: "", answer14: "", answer15: "", answer16: "", answer17: "", answer18: "" }
+const resetAsrs5State = { answer1: "", answer2: "", answer3: "", answer4: "", answer5: "", answer6: "" }
 
 const LocalData = ({ name, data, reset }) => {
     function save(name, data) {
@@ -15,6 +16,10 @@ const LocalData = ({ name, data, reset }) => {
 
         if (name === "asrs1") {
             reset(resetAsrs1State)
+        }
+
+        if (name === "asrs5") {
+            reset(resetAsrs5State)
         }
     }
 

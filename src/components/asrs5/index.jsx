b2705a3df77952c2e4e9783137759c64ce700e02
vishalicious213 <vishalicious213@gmail.com>
2024-04-16 13:34:45 -0700

    /asrs5/index: created asrs5context. weapped asrs5 components with it. exported it. moved formData state to it and shared as value

--- src/components/asrs5/index.jsx
+++ src/components/asrs5/index.jsx
@@ -1,15 +1,28 @@
+import React from 'react'
+import { useState, createContext } from "react"
+
 import Instructions from "./Instructions"
 import Form from "./Form"
 
-import React from 'react'
+const Asrs5Context = createContext()
 
 const Asrs5 = () => {
+    const [formData, setFormData] = useState({
+        answer1: "",
+        answer2: "",
+        answer3: "",
+        answer4: "",
+        answer5: "",
+        answer6: "",
+    })
+
     return (
-        <>
+        <Asrs5Context.Provider value={{formData, setFormData}}>
             <Instructions />
             <Form />
-        </>
+        </Asrs5Context.Provider>
     )
 }
 
 export default Asrs5
+export { Asrs5Context }

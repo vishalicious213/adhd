e39b0cbcc8c9565f5bee38437465b30c019dd44d
vishalicious213 <vishalicious213@gmail.com>
2024-04-16 14:13:39 -0700

    SaveAndDelete: imported useContext and Asrs5Context. used setComplete to change ASRS5 score completion to false when delete button is clicked

--- src/utilities/saveLocalData.jsx
+++ src/utilities/saveLocalData.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { useContext } from 'react'
+import { Asrs5Context } from '../components/asrs5'
 
 const resetAsrs1State = { answer1: "", answer2: "", answer3: "", answer4: "", answer5: "", answer6: "", answer7: "", answer8: "", answer9: "", answer10: "", answer11: "", answer12: "", answer13: "", answer14: "", answer15: "", answer16: "", answer17: "", answer18: "" }
 const resetAsrs5State = { answer1: "", answer2: "", answer3: "", answer4: "", answer5: "", answer6: "" }
 
 const SaveAndDelete = ({ name, data, reset }) => {
+    const { setComplete } = useContext(Asrs5Context)
+
     function save(name, data) {
         // console.log("SAVE TO LOCALSTORAGE", name, data)
         localStorage.setItem(name, JSON.stringify(data))
@@ -16,10 +20,12 @@ const SaveAndDelete = ({ name, data, reset }) => {
 
         if (name === "asrs1") {
             reset(resetAsrs1State)
+            setComplete(false)
         }
 
         if (name === "asrs5") {
             reset(resetAsrs5State)
+            setComplete(false)
         }
     }
 

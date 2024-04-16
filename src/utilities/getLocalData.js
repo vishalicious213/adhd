2775594695c108ac117100815da9350dab43d9b5
vishalicious213 <vishalicious213@gmail.com>
2024-04-15 21:16:44 -0700

    getLocalData: changed to jsx and added render component with buttons. save and load functions are inside of LocalData component

--- src/utilities/getLocalData.js
+++
@@ -1,8 +0,0 @@
-export const save = (name, data) => {
-    console.log("SAVE TO LOCALSTORAGE", name, data)
-    localStorage.setItem(name, JSON.stringify(data))
-}
-
-export const load = (name) => {
-    console.log("LOAD FROM LOCALSTORAGE", name)
-}
---
+++ src/utilities/getLocalData.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const LocalData = ({ name, data }) => {
+    function save(name, data) {
+        console.log("SAVE TO LOCALSTORAGE", name, data)
+        localStorage.setItem(name, JSON.stringify(data))
+    }
+    
+    function load(name) {
+        console.log("LOAD FROM LOCALSTORAGE", name)
+    }
+
+    return (
+        <div id="score">
+            <button className="score-btn" onClick={() => save(name, data)}>SAVE</button>
+            <button className="score-btn" onClick={() => load(name)}>LOAD</button>
+        </div>
+    )
+}
+
+export default LocalData

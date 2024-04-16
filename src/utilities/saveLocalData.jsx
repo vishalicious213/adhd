8cd8a1b1066ccaeef4e8113828fc78005a855b89
vishalicious213 <vishalicious213@gmail.com>
2024-04-16 10:52:47 -0700

    saveLocalData: renamed asrs1State to resetAsrs1State & made it one line. commented out console.logs

--- src/utilities/saveLocalData.jsx
+++ src/utilities/saveLocalData.jsx
@@ -1,39 +1,20 @@
 import React from 'react'
 
-const asrs1State = {
-    answer1: "",
-    answer2: "",
-    answer3: "",
-    answer4: "",
-    answer5: "",
-    answer6: "",
-    answer7: "",
-    answer8: "",
-    answer9: "",
-    answer10: "",
-    answer11: "",
-    answer12: "",
-    answer13: "",
-    answer14: "",
-    answer15: "",
-    answer16: "",
-    answer17: "",
-    answer18: "",
-}
+const resetAsrs1State = { answer1: "", answer2: "", answer3: "", answer4: "", answer5: "", answer6: "", answer7: "", answer8: "", answer9: "", answer10: "", answer11: "", answer12: "", answer13: "", answer14: "", answer15: "", answer16: "", answer17: "", answer18: "", }
 
 const LocalData = ({ name, data, reset }) => {
     function save(name, data) {
-        console.log("SAVE TO LOCALSTORAGE", name, data)
+        // console.log("SAVE TO LOCALSTORAGE", name, data)
         localStorage.setItem(name, JSON.stringify(data))
     }
 
     function remove(name, reset) {
-        console.log("DELETING FROM LOCALSTORAGE")
+        // console.log("DELETING FROM LOCALSTORAGE")
         localStorage.removeItem(name)
-        console.log(localStorage.getItem(name))
+        // console.log(localStorage.getItem(name))
 
         if (name === "asrs1") {
-            reset(asrs1State)
+            reset(resetAsrs1State)
         }
     }
 

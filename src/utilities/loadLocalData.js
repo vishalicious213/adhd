8ee4bc609d9b79658d4a08f7a5c2dbbe32b306cb
vishalicious213 <vishalicious213@gmail.com>
2024-04-17 13:32:38 -0700

    loadLocalData: cleaned up code

--- src/utilities/loadLocalData.js
+++ src/utilities/loadLocalData.js
@@ -1,11 +1,8 @@
 async function loadLocalData(name) {
-    console.log(name)
     if (localStorage.getItem(name)) {
         let savedData = await JSON.parse(localStorage.getItem(name))
-        console.log("real data", localStorage.getItem(name))
         return savedData
     } else {
-        console.log("No saved data found and loaded")
         return {}
     }
 }

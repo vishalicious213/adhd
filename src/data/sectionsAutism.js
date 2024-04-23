1369b29dbc79290762524ef220ad7396ad837b37
vishalicious213 <vishalicious213@gmail.com>
2024-04-22 17:20:09 -0700

    drafted /data/sectionsAutism.js

---
+++ src/data/sectionsAutism.js
@@ -0,0 +1,44 @@
+export const sectionsAdhdArray = [
+    {
+        name: "Autism",
+        def: "Autism spectrum disorder (ASD), or autism, is a developmental disability caused by differences in the brain. People with ASD often have problems with social communication and interaction, and restricted or repetitive behaviors or interests. People with ASD may also have different ways of learning, moving, or paying attention. It is important to note that some people without ASD might also have some of these symptoms. But for people with ASD, these characteristics can make life very challenging.",
+        tools: [
+            {
+                button: "ASRS v1.1", 
+                choice: "asrs1", 
+                title: "Adult ADHD Self-Report Scale (ASRS-v1.1) Symptom Checklist",
+                desc: "The ASRS v1.1 is an instrument consisting of 18 DSM-IV-TR criteria. Six of the eighteen questions were found to be the most predictive of symptoms consistent with ADHD. The remaining twelve provide additional cues and can serve as further probes into possible symptoms."
+            }, 
+            {
+                button: "ASRS v5", 
+                choice: "asrs5", 
+                title: "Adult ADHD Self-Report Scale (ASRS-v5) Symptom Checklist",
+                desc: "The ASRS5 is a self-report questionnaire designed to assess symptoms of ADHD in adults. It consists of 6 questions related to symptoms of inattention and hyperactivity / impulsivity. It is an updated version of the ASRS v1.1."
+            },
+            {
+                button: "SASI", 
+                choice: "sasi", 
+                title: "Women's ADHD Self-Assessment Symptom Inventory (SASI)",
+                desc: "Developed by two clinicians who focused on adult ADHD in women. SASI is not a diagnostic assessment. It gives insight into the types of challenges a woman with ADHD could experience. Its use is recommended as part of a structured interview in conjunction with other diagnostic tools."
+            }
+        ],
+        links: [
+            {
+                name: "Centers for Disease Control and Prevention: What is ADHD",
+                url: "https://www.cdc.gov/ncbddd/adhd/facts.html"
+            },
+            {
+                name: "National Institute of Mental Health: Attention-Deficit/Hyperactivity Disorder",
+                url: "https://www.nimh.nih.gov/health/topics/attention-deficit-hyperactivity-disorder-adhd"
+            },
+            {
+                name: "Psychiatry.org: What is ADHD",
+                url: "https://www.psychiatry.org/patients-families/adhd/what-is-adhd"
+            },
+            {
+                name: "Mayo Clinic: Adult attention-deficit/hyperactivity disorder (ADHD)",
+                url: "https://www.mayoclinic.org/diseases-conditions/adult-adhd/symptoms-causes/syc-20350878"
+            }
+        ]
+    }
+]

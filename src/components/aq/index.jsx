3d4c9991e3078c4a5425223342bd258b53e44122
vishalicious213 <vishalicious213@gmail.com>
2024-04-22 18:14:29 -0700

    drafted /aq/index.jsx

---
+++ src/components/aq/index.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { useState, createContext } from "react"
+
+// import Instructions from "./Instructions"
+// import Form from "./Form"
+// import SaveAndDelete from '../../utilities/saveLocalData'
+// import Score from './Score'
+
+const AQContext = createContext()
+
+const AQ = () => {
+    const [aqData, setAqData] = useState({})
+    // const [aqScore, setAqScore] = useState(0) 
+    // const [aqComplete, setAqComplete] = useState(false)
+
+    return (
+        <AQContext.Provider value={{
+            aqData,
+            setAqData
+        }}>
+            <h1>AQ</h1>
+            {/* <Instructions /> */}
+            {/* <Form /> */}
+            {/* <SaveAndDelete name="asrs1" data={asrs1Data} reset={setAsrs1Data} /> */}
+            {/* <Score scores={asrs1Data} /> */}
+        </AQContext.Provider>
+    )
+}
+
+export default AQ
+export { AQContext }

74fff690298aa5d9b44a7ad5e341735302cec4bd
vishalicious213 <vishalicious213@gmail.com>
2024-04-22 18:39:43 -0700

    updated /aq/Form.jsx. maps through questionsArray and renders them via RenderAqQuestions

--- src/components/aq/Form.jsx
+++ src/components/aq/Form.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import RenderAqQuestions from './RenderAqQuestions'
+import { questionsArray } from '../../data/aq'
 
 const Form = () => {
+    function handleChange(event) {
+        console.log(event)
+    }
+
     return (
-        <div>Form</div>
+        <>
+            {questionsArray.map(q => <RenderAqQuestions 
+                key={q.id}
+                question={q.text}
+                num={q.id}
+                handleChange={handleChange}
+            />)}
+        </>
     )
 }
 
---
+++ src/components/aq/RenderAqQuestions.jsx
@@ -0,0 +1,79 @@
+import React from 'react'
+
+const RenderAqQuestions = ({ question, num, checked, handleChange }) => {
+    return (
+        <>
+            <div className="question-container">
+                <div className="question-num">{num}</div>
+                <div className="text question">{question}</div>
+            </div>
+
+            <fieldset className="text">
+                {/* <legend>Frequency over past 6 months</legend> */}
+                
+                <div className="answer">
+                    <input 
+                        type="radio"
+                        id={`never${num}`}
+                        name={`answer${num}`} // same name for all; associates with same property in state
+                        value="never" // the value that will be saved to state
+                        checked={checked === "never"} // make it a controlled input
+                        onChange={handleChange} // used to connect to event handler
+                    />
+                    <label htmlFor={`never${num}`}>Never</label>
+                </div>
+
+                <div className="answer">
+                    <input 
+                        type="radio"
+                        id={`rarely${num}`}
+                        name={`answer${num}`}
+                        value="rarely"
+                        checked={checked === "rarely"}
+                        onChange={handleChange}
+                    />
+                    <label htmlFor={`rarely${num}`}>Rarely</label>
+                </div>
+
+                <div className="answer">
+                    <input 
+                        type="radio"
+                        id={`sometimes${num}`}
+                        name={`answer${num}`}
+                        value="sometimes"
+                        checked={checked === "sometimes"}
+                        onChange={handleChange}
+                    />
+                    <label htmlFor={`sometimes${num}`}>Sometimes</label>
+                </div>
+
+                <div className="answer">
+                    <input 
+                        type="radio"
+                        id={`often${num}`}
+                        name={`answer${num}`}
+                        value="often"
+                        checked={checked === "often"}
+                        onChange={handleChange}
+                    />
+                    <label htmlFor={`often${num}`}>Often</label>
+                </div>
+
+                <div className="answer">
+                    <input 
+                        type="radio"
+                        id={`very-often${num}`}
+                        name={`answer${num}`}
+                        value="very-often"
+                        checked={checked === "very-often"}
+                        onChange={handleChange}
+                    />
+                    <label htmlFor={`very-often${num}`}>Very Often</label>
+                </div>
+
+            </fieldset>
+        </>
+    )
+}
+
+export default RenderAqQuestions

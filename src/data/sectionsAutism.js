388e28a2d072eeb18be32a66199346240d27ac00
vishalicious213 <vishalicious213@gmail.com>
2024-04-22 17:32:55 -0700

    updated /data/sectionsAutism.js with links

--- src/data/sectionsAutism.js
+++ src/data/sectionsAutism.js
@@ -9,35 +9,39 @@ export const sectionsAutismArray = [
                 title: "Adult ADHD Self-Report Scale (ASRS-v1.1) Symptom Checklist",
                 desc: "The ASRS v1.1 is an instrument consisting of 18 DSM-IV-TR criteria. Six of the eighteen questions were found to be the most predictive of symptoms consistent with ADHD. The remaining twelve provide additional cues and can serve as further probes into possible symptoms."
             }, 
-            {
-                button: "ASRS v5", 
-                choice: "asrs5", 
-                title: "Adult ADHD Self-Report Scale (ASRS-v5) Symptom Checklist",
-                desc: "The ASRS5 is a self-report questionnaire designed to assess symptoms of ADHD in adults. It consists of 6 questions related to symptoms of inattention and hyperactivity / impulsivity. It is an updated version of the ASRS v1.1."
-            },
-            {
-                button: "SASI", 
-                choice: "sasi", 
-                title: "Women's ADHD Self-Assessment Symptom Inventory (SASI)",
-                desc: "Developed by two clinicians who focused on adult ADHD in women. SASI is not a diagnostic assessment. It gives insight into the types of challenges a woman with ADHD could experience. Its use is recommended as part of a structured interview in conjunction with other diagnostic tools."
-            }
+            // {
+            //     button: "ASRS v5", 
+            //     choice: "asrs5", 
+            //     title: "Adult ADHD Self-Report Scale (ASRS-v5) Symptom Checklist",
+            //     desc: "The ASRS5 is a self-report questionnaire designed to assess symptoms of ADHD in adults. It consists of 6 questions related to symptoms of inattention and hyperactivity / impulsivity. It is an updated version of the ASRS v1.1."
+            // },
+            // {
+            //     button: "SASI", 
+            //     choice: "sasi", 
+            //     title: "Women's ADHD Self-Assessment Symptom Inventory (SASI)",
+            //     desc: "Developed by two clinicians who focused on adult ADHD in women. SASI is not a diagnostic assessment. It gives insight into the types of challenges a woman with ADHD could experience. Its use is recommended as part of a structured interview in conjunction with other diagnostic tools."
+            // }
         ],
         links: [
             {
-                name: "Centers for Disease Control and Prevention: What is ADHD",
-                url: "https://www.cdc.gov/ncbddd/adhd/facts.html"
+                name: "Centers for Disease Control and Prevention: What is Autism Spectrum Disorder?",
+                url: "https://www.cdc.gov/ncbddd/autism/facts.html"
             },
             {
-                name: "National Institute of Mental Health: Attention-Deficit/Hyperactivity Disorder",
-                url: "https://www.nimh.nih.gov/health/topics/attention-deficit-hyperactivity-disorder-adhd"
+                name: "National Institute of Mental Health: Autism Spectrum Disorder",
+                url: "https://www.nimh.nih.gov/health/topics/autism-spectrum-disorders-asd"
             },
             {
-                name: "Psychiatry.org: What is ADHD",
-                url: "https://www.psychiatry.org/patients-families/adhd/what-is-adhd"
+                name: "Psychiatry.org: What Is Autism Spectrum Disorder?",
+                url: "https://www.psychiatry.org/patients-families/autism/what-is-autism-spectrum-disorder"
             },
             {
-                name: "Mayo Clinic: Adult attention-deficit/hyperactivity disorder (ADHD)",
-                url: "https://www.mayoclinic.org/diseases-conditions/adult-adhd/symptoms-causes/syc-20350878"
+                name: "Mayo Clinic: Autism spectrum disorder",
+                url: "https://www.mayoclinic.org/diseases-conditions/autism-spectrum-disorder/symptoms-causes/syc-20352928"
+            },
+            {
+                name: "Autism Speaks: Autism spectrum disorder (ASD)",
+                url: "https://www.autismspeaks.org/what-autism"
             }
         ]
     }

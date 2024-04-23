1bcf64df58f6e3ef6a56989c92077f64c8807fa5
vishalicious213 <vishalicious213@gmail.com>
2024-04-22 19:04:14 -0700

    /aq/Form.jsx: imported useContext. imported AQContext. added aqData and setAqData from context. added 'checked' prop to RenderAqQuestions

--- src/components/aq/Form.jsx
+++ src/components/aq/Form.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { useContext } from 'react'
+import { AQContext } from '.'
 import RenderAqQuestions from './RenderAqQuestions'
 import { questionsArray } from '../../data/aq'
 
 const Form = () => {
+    const { aqData, setAqData } = useContext(AQContext)
+
     function handleChange(event) {
         console.log(event)
     }
@@ -13,6 +17,7 @@ const Form = () => {
                 key={q.id}
                 question={q.text}
                 num={q.id}
+                checked={Object.values(aqData)[q.id - 1]}
                 handleChange={handleChange}
             />)}
         </>

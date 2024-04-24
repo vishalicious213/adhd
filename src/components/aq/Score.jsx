f8bda2bf289eb2ab571938c051a97cf5bf6fe7fb
vishalicious213 <vishalicious213@gmail.com>
2024-04-23 23:25:59 -0700

    aq/Score: updating aqScore from workingScore when button is clicked & rendering it

--- src/components/aq/Score.jsx
+++ src/components/aq/Score.jsx
@@ -8,16 +8,15 @@ const Score = ({ scores }) => {
     const agree = [2, 4, 5, 6, 7, 9, 12, 13, 16, 18, 19, 20, 21, 22, 23, 26, 33, 35, 39, 41, 42, 43, 45, 46]
     const disagree = [1, 3, 8, 10, 11, 14, 15, 17, 24, 25, 27, 28, 29, 30, 31, 32, 34, 36, 37, 38, 40, 44, 47, 48, 49, 50]
     let workingScore = 0
-    // let unfinishedQuestions = 50
     let answers = Object.values(scores)
 
-        function getScore() {
-            setAqComplete(false)
-            setUnfinishedQuestions(50 - answers.length)
-            
-            if (answers.length === 50 && !answers.includes("")) {
-                setAqComplete(true)
-                console.log("COMPLETE")
+    function getScore() {
+        setAqComplete(false)
+        setUnfinishedQuestions(50 - answers.length)
+        
+        if (answers.length === 50 && !answers.includes("")) {
+            setAqComplete(true)
+            console.log("COMPLETE")
         }
 
         for (let i = 0; i < 50; i++) {
@@ -35,15 +34,16 @@ const Score = ({ scores }) => {
                 }
             }
         }
+
+        setAqScore(workingScore)
     }
 
     return (
-        <div>
-            <div>Autism Score</div>
-            <button onClick={getScore}>GET SCORE</button>
-            <div>
+        <div id="score">
+            <button className='score-btn' onClick={getScore}>GET SCORE</button>
+            <div className='text'>
                 { !aqComplete ? `Please answer remaining ${unfinishedQuestions} questions`
-                : `Score is ${workingScore}`
+                : `Score is ${aqScore}`
                 }
             </div>
         </div>

ce7420f1c91e267a36467a02048fa297510bb644
vishalicious213 <vishalicious213@gmail.com>
2024-04-23 22:41:10 -0700

    aq/Score: calculating if form is complete

--- src/components/aq/Score.jsx
+++ src/components/aq/Score.jsx
@@ -4,40 +4,55 @@ import { AQContext } from '.'
 
 const Score = ({ scores }) => {
     const { aqScore, setAqScore, aqComplete, setAqComplete } = useContext(AQContext)
-    let workingScore = 0
-
-    console.log(scores)
-    // console.log(scores.answer50)
     const agree = [2, 4, 5, 6, 7, 9, 12, 13, 16, 18, 19, 20, 21, 22, 23, 26, 33, 35, 39, 41, 42, 43, 45, 46]
     const disagree = [1, 3, 8, 10, 11, 14, 15, 17, 24, 25, 27, 28, 29, 30, 31, 32, 34, 36, 37, 38, 40, 44, 47, 48, 49, 50]
+    let workingScore = 0
+    let unfinishedQuestions = 50
+    let answers = Object.values(scores)
 
-    for (let i = 0; i < 50; i++) {
-        let item = `answer${i + 1}`
-        // console.log(item)
-        // console.log(scores[item])
-
-        // if (agree.includes(i + 1)) {
-        //     console.log(i+1, "agree", scores[item])
-        // } else {
-        //     console.log(i+1, "disagree", scores[item])
-        // }
-
-        if (agree.includes(i + 1)) {
-            if (scores[item] === "def-agree" || scores[item] === "slight-agree") {
-                console.log(item,scores[item])
-                workingScore = workingScore + 1
-            }
-        }
-
-        if (disagree.includes(i + 1)) {
-            if (scores[item] === "def-disagree" || scores[item] === "slight-disagree") {
-                console.log(item,scores[item])
-                workingScore = workingScore + 1
-            }
-        }
-
-        console.log("working score", workingScore)
+    console.log(scores)
+    console.log(answers)
+    console.log(answers.length)
+    if (answers.length === 50 && !answers.includes("")) {
+        console.log("COMPLETE")
+    } else {
+        console.log(unfinishedQuestions - answers.length, "QUESTIONS LEFT")
     }
+
+    // function checkComplete() {
+    // }
+
+    function getScore() {
+        for (let i = 0; i < 50; i++) {
+            let item = `answer${i + 1}`
+            // console.log(item)
+            // console.log(scores[item])
+
+            // if (agree.includes(i + 1)) {
+            //     console.log(i+1, "agree", scores[item])
+            // } else {
+            //     console.log(i+1, "disagree", scores[item])
+            // }
+
+            if (agree.includes(i + 1)) {
+                if (scores[item] === "def-agree" || scores[item] === "slight-agree") {
+                    console.log(item,scores[item])
+                    workingScore = workingScore + 1
+                }
+            }
+
+            if (disagree.includes(i + 1)) {
+                if (scores[item] === "def-disagree" || scores[item] === "slight-disagree") {
+                    console.log(item,scores[item])
+                    workingScore = workingScore + 1
+                }
+            }
+
+            console.log("working score", workingScore)
+        }
+    }
+
+    getScore()
 
     return (
         <div>

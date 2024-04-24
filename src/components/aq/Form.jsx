002a77a9fb713728b1de4c916961d40da38245ab
vishalicious213 <vishalicious213@gmail.com>
2024-04-24 16:41:43 -0700

    aq/index: set up useEfect to load saved data on form load. imported loadLocalData

--- src/components/aq/Form.jsx
+++ src/components/aq/Form.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
-import { useContext } from 'react'
+import { useContext, useEffect } from 'react'
 import { AQContext } from '.'
 import RenderAqQuestions from './RenderAqQuestions'
+import loadLocalData from '../../utilities/loadLocalData'
 import { questionsArray } from '../../data/aq'
 
 const Form = () => {
     const { setAqData } = useContext(AQContext)
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const savedData = await loadLocalData("aq")
+                setAqData(savedData)
+            } catch (error) {
+                console.error("No saved data found for AQ tool")
+            }
+        }
+
+        fetchData()
+    }, [])
 
     function handleChange(event) {
         const { name, value } = event.target

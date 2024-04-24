952c335cbc04c06ab2eb4a195851e1a971d5f04d
vishalicious213 <vishalicious213@gmail.com>
2024-04-24 14:12:35 -0700

    CSS: styling radio-button forms

--- src/components/aq/RenderAqQuestions.jsx
+++ src/components/aq/RenderAqQuestions.jsx
@@ -12,57 +12,57 @@ const RenderAqQuestions = ({ question, num, checked, handleChange }) => {
                 <div className="text question">{question}</div>
             </div>
 
-            <fieldset className="text">
-                {/* <legend>Frequency over past 6 months</legend> */}
-                
-                <div className="answer">
-                    <input 
-                        type="radio"
-                        id={`def-agree${num}`}
-                        name={`answer${num}`} // same name for all; associates with same property in state
-                        value="def-agree" // the value that will be saved to state
-                        checked={aqData[checked] === "def-agree"} // make it a controlled input
-                        onChange={handleChange} // used to connect to event handler
-                    />
-                    <label htmlFor={`def-agree${num}`}>Definitely agree</label>
-                </div>
+            {/* <fieldset className="text"> */}
+                <div className="answer-wrapper text">
+                    <div className="answer agree-color">
+                        <input 
+                            type="radio"
+                            id={`def-agree${num}`}
+                            name={`answer${num}`} // same name for all; associates with same property in state
+                            value="def-agree" // the value that will be saved to state
+                            checked={aqData[checked] === "def-agree"} // make it a controlled input
+                            onChange={handleChange} // used to connect to event handler
+                        />
+                        <label htmlFor={`def-agree${num}`}>Definitely agree</label>
+                    </div>
 
-                <div className="answer">
-                    <input 
-                        type="radio"
-                        id={`slight-agree${num}`}
-                        name={`answer${num}`}
-                        value="slight-agree"
-                        checked={aqData[checked] === "slight-agree"}
-                        onChange={handleChange}
-                    />
-                    <label htmlFor={`slight-agree${num}`}>Slightly agree</label>
-                </div>
+                    <div className="answer agree-color">
+                        <input 
+                            type="radio"
+                            id={`slight-agree${num}`}
+                            name={`answer${num}`}
+                            value="slight-agree"
+                            checked={aqData[checked] === "slight-agree"}
+                            onChange={handleChange}
+                        />
+                        <label htmlFor={`slight-agree${num}`}>Slightly agree</label>
+                    </div>
 
-                <div className="answer">
-                    <input 
-                        type="radio"
-                        id={`slight-disagree${num}`}
-                        name={`answer${num}`}
-                        value="slight-disagree"
-                        checked={aqData[checked] === "slight-disagree"}
-                        onChange={handleChange}
-                    />
-                    <label htmlFor={`slight-disagree${num}`}>Slightly disagree</label>
-                </div>
+                    <div className="answer disagree-color">
+                        <input 
+                            type="radio"
+                            id={`slight-disagree${num}`}
+                            name={`answer${num}`}
+                            value="slight-disagree"
+                            checked={aqData[checked] === "slight-disagree"}
+                            onChange={handleChange}
+                        />
+                        <label htmlFor={`slight-disagree${num}`}>Slightly disagree</label>
+                    </div>
 
-                <div className="answer">
-                    <input 
-                        type="radio"
-                        id={`def-disagree${num}`}
-                        name={`answer${num}`}
-                        value="def-disagree"
-                        checked={aqData[checked] === "def-disagree"}
-                        onChange={handleChange}
-                    />
-                    <label htmlFor={`def-disagree${num}`}>Definitely disagree</label>
+                    <div className="answer disagree-color">
+                        <input 
+                            type="radio"
+                            id={`def-disagree${num}`}
+                            name={`answer${num}`}
+                            value="def-disagree"
+                            checked={aqData[checked] === "def-disagree"}
+                            onChange={handleChange}
+                        />
+                        <label htmlFor={`def-disagree${num}`}>Definitely disagree</label>
+                    </div>
                 </div>
-            </fieldset>
+            {/* </fieldset> */}
         </>
     )
 }

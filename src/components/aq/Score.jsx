f854c0be019c67c871d904cf442770b8ffa62d9b
vishalicious213 <vishalicious213@gmail.com>
2024-04-26 10:40:35 -0700

    /aq/Score: declared subscale score variables

--- src/components/aq/Score.jsx
+++ src/components/aq/Score.jsx
@@ -8,6 +8,11 @@ const Score = ({ scores }) => {
     const agree = [2, 4, 5, 6, 7, 9, 12, 13, 16, 18, 19, 20, 21, 22, 23, 26, 33, 35, 39, 41, 42, 43, 45, 46]
     const disagree = [1, 3, 8, 10, 11, 14, 15, 17, 24, 25, 27, 28, 29, 30, 31, 32, 34, 36, 37, 38, 40, 44, 47, 48, 49, 50]
     let workingScore = 0
+    let socialScore = 0
+    let attentionSwitchingScore = 0
+    let attentionToDetailScore = 0
+    let communicationScore = 0
+    let imaginationScore = 0
     let answers = Object.values(scores)
 
     function getScore() {

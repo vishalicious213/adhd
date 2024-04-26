09bb562d62cd533ce06d56aa8d356ccb7cb20f23
vishalicious213 <vishalicious213@gmail.com>
2024-04-26 10:53:36 -0700

    /aq/Score: declared subscale item arrays

--- src/components/aq/Score.jsx
+++ src/components/aq/Score.jsx
@@ -8,11 +8,16 @@ const Score = ({ scores }) => {
     const agree = [2, 4, 5, 6, 7, 9, 12, 13, 16, 18, 19, 20, 21, 22, 23, 26, 33, 35, 39, 41, 42, 43, 45, 46]
     const disagree = [1, 3, 8, 10, 11, 14, 15, 17, 24, 25, 27, 28, 29, 30, 31, 32, 34, 36, 37, 38, 40, 44, 47, 48, 49, 50]
     let workingScore = 0
-    let socialScore = 0
+    let socialSkillScore = 0
     let attentionSwitchingScore = 0
     let attentionToDetailScore = 0
     let communicationScore = 0
     let imaginationScore = 0
+    const socialSkillArr = [1,11,13,15,22,36,44,45, 47,48]
+    const attentionSwitchingArr = [2,4,10,16,25,32,34, 37,43,46]
+    const attentionToDetailArr = [5,6,9,12,19,23,28, 29,30,49]
+    const communicationArr = [7,17,18,26,27,31,33, 35,38,39]
+    const imaginationArr = [3,8,14,20,21,24,40,41,42,50]
     let answers = Object.values(scores)
 
     function getScore() {

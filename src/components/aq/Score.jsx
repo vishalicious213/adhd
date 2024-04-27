f86ea07caf0e49eda40797a62f3b6181a6d5c9e2
vishalicious213 <vishalicious213@gmail.com>
2024-04-26 17:18:07 -0700

    /aq/Score: set up subscore state

--- src/components/aq/Score.jsx
+++ src/components/aq/Score.jsx
@@ -4,6 +4,13 @@ import { AQContext } from '.'
 
 const Score = ({ scores }) => {
     const { aqScore, setAqScore, aqComplete, setAqComplete } = useContext(AQContext)
+    const { subscore, setSubscore} = useState({
+        social: 0,
+        attentionSwitching: 0,
+        attentionToDetail: 0,
+        communication: 0,
+        imagination: 0
+    })
     const [unfinishedQuestions, setUnfinishedQuestions] = useState(50)
     const agree = [2, 4, 5, 6, 7, 9, 12, 13, 16, 18, 19, 20, 21, 22, 23, 26, 33, 35, 39, 41, 42, 43, 45, 46]
     const disagree = [1, 3, 8, 10, 11, 14, 15, 17, 24, 25, 27, 28, 29, 30, 31, 32, 34, 36, 37, 38, 40, 44, 47, 48, 49, 50]

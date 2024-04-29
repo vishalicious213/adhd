1e7177446887ae4f5e40d3037f466d9279fa334f
vishalicious213 <vishalicious213@gmail.com>
2024-04-29 11:11:05 -0700

    /aq/Score: added showScore, setShowScore state (to toggle score modal)

--- src/components/aq/Score.jsx
+++ src/components/aq/Score.jsx
@@ -12,6 +12,7 @@ const Score = ({ scores }) => {
         imagination: 0
     })
     const [unfinishedQuestions, setUnfinishedQuestions] = useState(50)
+    const [showScore, setShowScore] = useState(false)
     const agree = [2, 4, 5, 6, 7, 9, 12, 13, 16, 18, 19, 20, 21, 22, 23, 26, 33, 35, 39, 41, 42, 43, 45, 46]
     const disagree = [1, 3, 8, 10, 11, 14, 15, 17, 24, 25, 27, 28, 29, 30, 31, 32, 34, 36, 37, 38, 40, 44, 47, 48, 49, 50]
     let workingScore = 0
@@ -150,7 +151,7 @@ const Score = ({ scores }) => {
     return (
         <div id="score">
             <button className='save-btn calc-btn' title='Calculate score' onClick={getScore}>%</button>
-            <section className='text aq-score'>
+            <section className='text aq-score hide'>
                 { !aqComplete ? `Please answer remaining ${unfinishedQuestions} questions`
                 : <div>
                     <h3>{`Score is ${aqScore}`}</h3>
